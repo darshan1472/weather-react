815fae2c6aaea2bed4de77b1a7d3eb35c475a255
darshan1472 <87933427+darshan1472@users.noreply.github.com>
2024-07-11 17:36:59 +0530

    Update WeatherCard.js
    added ./

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -5,12 +5,12 @@ const WeatherCard = ({ weather }) => {
   const getBackgroundImage = () => {
     if (!weather) return "";
     const main = weather.weather[0].main.toLowerCase();
-    if (main.includes("clouds")) return "url(/images/cloudy.jpg)";
+    if (main.includes("clouds")) return "url(./images/cloudy.jpg)";
     if (main.includes("drizzle") || main.includes("rain"))
-      return "url(/images/rainy.jpg)";
-    if (main.includes("clear")) return "url(/images/sunny.jpg)";
+      return "url(./images/rainy.jpg)";
+    if (main.includes("clear")) return "url(./images/sunny.jpg)";
     if (main.includes("snow") || main.includes("cold"))
-      return "url(/images/snowy.jpg)";
+      return "url(./images/snowy.jpg)";
     return "";
   };
 
